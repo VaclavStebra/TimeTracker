f172ad8a7d16e05d8638e9eafec2bbb3f8b24a8b
Vaclav Stebra <vstebra@gmail.com>
2018-01-21 00:24:03 +0100

    Signup, login, logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Logged in!"
+      redirect_to welcome_index_path
+    else
+      flash[:alert] = "Email or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:succes] = "Logged out"
+    redirect_to welcome_index_path
+  end
+end
